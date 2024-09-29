a489d9af6708d77c9df391576ef19de62bdf24e7
AliOrionCor <khan.ali0b@gmail.com>
2024-09-29 17:41:29 -0230

    createAPI

--- controllers/adminController.ts
+++ controllers/adminController.ts
@@ -3,31 +3,37 @@ import { Vandor } from "../models/vandor";
 
 export const createVendor = async (req, res) => {
 
+    console.log('hello')
 
     const { name, ownerName, address, foodType, pinCode, phone, email, password } = <createVandorInput>req.body;
+    try {
+        const createVandor = await Vandor.create({
+            name: name,
+            ownerName: ownerName,
+            address: address,
+            foodType: foodType,
+            pinCode: pinCode,
+            phone: phone,
+            email: email,
+            password: password,
+            salt: '123213',
+            coverImage: [],
+            rating: 0,
+            serviceAvailable: false,
 
+        })
 
-    const createVandor = await Vandor.create({
-        name: name,
-        ownerName: ownerName,
-        address: address,
-        foodType: foodType,
-        pinCode: pinCode,
-        phone: phone,
-        email: email,
-        password: password,
-        salt:'',
-        coverImage:[],
-        rating:0,
-        serviceAvailable:false,
+        return res.json({
+            message: 'Vandor created successfull',
+            data: createVandor
+        })
 
-    })
+    } catch (error) {
+        return res.json({
+            message: error.message
+        })
 
-
-    return res.json({
-        message: 'Vandor created successfull',
-        data: createVandor
-    })
+    }
 };
 
 export const getVandor = async (req, res) => {
